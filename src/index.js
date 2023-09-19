b37ad17ce661c7678fd024c7baed2fb8603fbd49
daniel-lopes-98435 <daniel_slopes@hotmail.com>
2023-09-19 16:14:50 -0300

    firt-commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReacDOM from 'react-dom/client';
+
+import App from './components/app';
+
+const root = ReacDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <App />
+);
