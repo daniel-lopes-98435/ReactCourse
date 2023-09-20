9141a6c569ff4d2dc63fc9aca7311d101bf89a4f
daniel-lopes-98435 <daniel_slopes@hotmail.com>
2023-09-20 11:41:54 -0300

    downgrade react 17

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import ReacDOM from 'react-dom/client';
+import ReacDOM from 'react-dom';
 
 import App from './components/app';
 
-const root = ReacDOM.createRoot(document.getElementById('root'));
-
-root.render(
-    <App />
-);
+ReacDOM.render(<App />, document.getElementById('root'));
